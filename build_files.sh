68b29558ac8a5cc587b20cd372b0e8ad73ff5657
BezerraC <cbezerraneto@gmail.com>
2024-01-24 14:51:34 -0300

    fix posApp static

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,6 @@
  
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
+ python3.9 manage.py makemigrations
  python3.9 manage.py migrate 
  echo "BUILD END"
